3d7ed17b8c4d26e33122d69f7d52bc59a74a0c60
Shannen Bendeck <shay@macbook-pro.myfiosgateway.com>
2022-05-16 21:53:17 -0400

    formatting body

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,43 @@
+function question1() {
+    var questions = $("#questions")
+
+    questions.text("what color is the sky?")
+var answer = "blue"
+// if wrong 
+// timer count -=2 
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var timerCount = 75 
+
 function startTimer() { 
     var startButton = $("#startButton");
     console.log (startButton)
 
     var timeDisplay =$(".time")
-    var timerCount = 75 
      setInterval(function() {
          timerCount--;
      
          timeDisplay.text(timerCount)
          console.log(timerCount)       
  }, 1000);
-
-
-
-
+ question1()
 }
-
