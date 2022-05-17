de07d19e853580ce7b385d88ec53858bf5afb0a7
Shannen Bendeck <shay@macbook-pro.myfiosgateway.com>
2022-05-16 23:56:09 -0400

    added questions

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,34 @@
 function question1() {
     var questions = $("#questions")
-
-    questions.text("what color is the sky?")
-var answer = "blue"
-// if wrong 
-// timer count -=2 
-
-
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const questions = [
+    {
+        question: "Commonly used data types DO NOT include:",
+        choices: ["1. stings","2. booleans","3. alerts","4. numbers"],
+        answer: "3. alerts"
+    },
+    {
+        question: "The condition in an if/else statment is enclosed within ___.",
+        choices:["1. quotes","2. curly brackets","3. parentheses","4. square brackets"],
+        answer: "3. parentheses"
+    },
+    {
+        question: "Arrays in JavaScript can be used to store ___.",
+        choices:["1. numbers and strings","2. other arrays","3. booleans","4. all of the above"],
+        answer: "4. all of the above"
+    },
+    {
+        question: "Strings values must be enclosed within ___ when being assigned to variables.",
+        choices:["1. commas","2. curly brackets","3. quotes","4. parentheses"],
+        answer: "3. quotes"
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: ["1. javascript","2. terminal/bash","3. for loops","4. console.log"],
+        answer: "4. console.log"
+    }
+];
 
 
 var timerCount = 75 
@@ -32,7 +37,7 @@ function startTimer() {
     var startButton = $("#startButton");
     console.log (startButton)
 
-    var timeDisplay =$(".time")
+    var timeDisplay =$(".timer")
      setInterval(function() {
          timerCount--;
      
